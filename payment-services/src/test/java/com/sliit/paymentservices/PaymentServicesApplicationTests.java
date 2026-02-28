0fb9a1c9cac0aa9524acb577daac5a029e4f096e
thewandamnidu <thewan2001@gmail.com>
2026-02-28 16:12:46 +0530

    chore : initial setup

---
+++ payment-services/src/test/java/com/sliit/paymentservices/PaymentServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.paymentservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
